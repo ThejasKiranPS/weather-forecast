e8feb18dda086e74b67703fa685fae0179680066
thejaskiranps <thejaskiranps@gmail.com>
2022-01-09 21:54:10 +0530

    finish search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,32 +1,48 @@
 import react from "react";
-import style from './search.module.css';
-import className from '../../className';
+import style from "./search.module.css";
+import className from "../../className";
+import {FaSearch} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
 
 class Search extends react.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ""
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+  }
 
-    handleChange = (e) => {
-        this.setState({value: e.target.value});
-    }
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.handleSubmit(this.state.value);
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.value);
+  };
 
-    render() {
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <button type="submit" className={className(style.searchBg)} >submit</button>
-                <input value={this.state.value} onChange={this.handleChange} />
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className={className(style.container)}>
+        <button 
+            type="submit" 
+            className={className(style.searchBg)} 
+        >
+            <div className={style.iconContainer}> 
+                <IconContext.Provider value={{className: style.icon}}>
+                    <FaSearch style={style.icons} />
+                </IconContext.Provider>
+            </div>
+        </button>
+
+        <input 
+            className={className(style.searchFg)}
+            value={this.state.value} 
+            onChange={this.handleChange} 
+        />
+      </form>
+    );
+  }
 }
 
 export default Search;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,9 +3,20 @@ import style from './home.module.css';
 import Search from '../../components/search/search';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: ''
+        }
+    }
+
+
+
     render() {
         return(
-            <></>
+            <>
+                <Search onSubmit={(value) => this.setState({location: value})} />
+            </>
         );
     }
 }
