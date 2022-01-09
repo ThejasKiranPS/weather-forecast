ed161839e189ecc24778d8e1898ce35d4525d477
thejaskiranps <thejaskiranps@gmail.com>
2022-01-09 09:47:15 +0530

    init pages structure

---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './home.module.css';
+import Search from '../../components/search/search';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <></>
+        );
+    }
+}
+
+export default Home;
