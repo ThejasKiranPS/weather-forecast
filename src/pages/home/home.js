3b868444863c1dc6ed12b5e2686693dc57718c93
thejaskiranps <thejaskiranps@gmail.com>
2022-01-10 10:01:35 +0530

    fetch data

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,19 +3,27 @@ import style from './home.module.css';
 import Search from '../../components/search/search';
 
 class Home extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
             location: ''
         }
     }
+    geturl = (location) => `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=8771e349f3087c578d3ad5cadef7ebe3`
 
-
+    onSubmit = async (location) => {
+        this.setState({location});
+        console.log(this.geturl(location))
+        const result = await fetch(this.geturl(location));
+        const data = await result.json();
+    }
 
     render() {
         return(
             <>
-                <Search onSubmit={(value) => this.setState({location: value})} />
+                <Search onSubmit={this.onSubmit} />
+
             </>
         );
     }
