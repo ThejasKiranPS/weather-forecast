107336b3568731f702b2eb4aa3f956a5a482a47a
thejaskiranps <thejaskiranps@gmail.com>
2022-01-10 10:36:07 +0530

    add location text

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import style from './home.module.css';
 import Search from '../../components/search/search';
+import { MdLocationPin } from 'react-icons/md';
 
 class Home extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            location: ''
+            location: 'London'
         }
     }
     geturl = (location) => `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=8771e349f3087c578d3ad5cadef7ebe3`
 
-    onSubmit = async (location) => {
-        this.setState({location});
-        console.log(this.geturl(location))
-        const result = await fetch(this.geturl(location));
+    fetchData = async () => {
+        console.log(this.geturl(this.state.location))
+        const result = await fetch(this.geturl(this.state.location));
         const data = await result.json();
     }
 
     render() {
         return(
             <>
-                <Search onSubmit={this.onSubmit} />
+                <Search onSubmit={(location) => this.setState({location}, this.fetchData)} />
+                <span className={style.locationContainer}>
+                    <MdLocationPin />
+                    <p>{this.state.location}</p>
+                </span>
 
             </>
         );
